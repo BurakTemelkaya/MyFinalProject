1295deb92dda5e90de318eb1d07bfdbbc5f07074
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-03-23 23:06:14 +0300

    Ders 11 tamamlandı.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Entites.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productservice;
+
+        public ProductsController(IProductService productService)
+        {
+            _productservice = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _productservice.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            var result = _productservice.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            var result = _productservice.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
